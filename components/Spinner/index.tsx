df5aa5774cf067d51f2b2642b67021a6c930a129
basem <basemsisy@gmail.com>
2021-10-24 03:09:07 +0200

    feat[Spinner]: loadig spinner

---
+++ components/Spinner/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./Spinner.module.scss";
+
+const Spinner = (): JSX.Element => {
+  return (
+    <div className={styles.Spinner}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Spinner;
